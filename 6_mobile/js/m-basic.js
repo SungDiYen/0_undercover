9ebd4d2c0859c5222cdfb1eb652e076aa9855c4f
SungDiYen <ba_la_888@hotmail.com>
2015-01-30 16:15:29 +0800

    i9u9up

--- 6_mobile/js/m-basic.js
+++ 6_mobile/js/m-basic.js
@@ -1,13 +1,18 @@
 $(function(){
 	$('.search-area').hide();
 	$('#search-btn').click(function() {
-		$('.search-area').removeClass('slideInLeft').addClass('slideInRight');
-		$('.search-area').show(300);
+		$('.search-area').removeClass('fadeOut').addClass('fadeIn').show()
+		.animate({right:'0px'}, 300);
+		$('#search-bar').addClass('bounceInRight');
 		$('body').append('<div id="sidenav-overlay"></div>');
+		$(this).hide(100);
 		
 		$('#search-btn2, #sidenav-overlay').click(function(){
 			$('#sidenav-overlay').remove();
-			$('.search-area').removeClass('slideInRight').addClass('slideInLeft').hide();
+			$('.search-area').removeClass('fadeIn').addClass('fadeOut')
+			.animate({right:'-230px'}, 300).hide(300);
+			$('#search-bar').removeClass('bounceInRight');
+			$('#search-btn').show(750);
 		});
 	});
 });
